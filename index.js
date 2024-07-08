23191b1778790fbea3f296aa27c611a38fa34a65
vivianzhou1288 <vivianzhou1288@gmail.com>
2024-07-07 20:06:01 -0400

    added configurations

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 /* CONFIGURATIONS */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan);
